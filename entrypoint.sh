618bf2037a33a53ec50c5526af18f74207b9485e
Daniel Yakov <daniel577000@gmail.com>
2022-12-20 17:50:58 +0200

    added mysql

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-export DB_DIALECT=HSQLDB
-export DB_URL=jdbc:hsqldb:file:lavagna
-export DB_USER=sa
-export DB_PASS=""
+export DB_DIALECT=MYSQL
+export DB_URL='jdbc:mysql://mysql_db:3306/lavagna?useSSL=false&allowPublicKeyRetrieval=true'
+export DB_USER=root
+export DB_PASS=password
 export SPRING_PROFILE=dev
-
-
 
 java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
 -Ddatasource.url="${DB_URL}" \
